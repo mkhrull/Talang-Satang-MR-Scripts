885c349cf35db836bde2d1ab3224f8d7dabff015
Muhammad Khairul <56944821+mkhrull@users.noreply.github.com>
2024-05-21 16:58:48 +0800

    Update SceneSwitcher.cs comments

--- SceneSwitcher.cs
+++ SceneSwitcher.cs
@@ -12,16 +12,19 @@ public class SceneSwitcher : MonoBehaviour
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - currentSceneIndex);
     }
 
+    // Scene name is set in Unity3D
     public void MoveToScene(string sceneName)
     {
         SceneManager.LoadScene(sceneName);
     }
 
+    // For QC purposes
     public void btnTest()
     {
         Debug.Log(EventSystem.current.currentSelectedGameObject.name);
     }
 
+    // Function only works in HoloLens 2, in unity3d can be checked using debug.log
     public void ExitApp()
     {
         Application.Quit();
